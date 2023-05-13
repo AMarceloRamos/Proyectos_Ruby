436dc22ba09fe7cea5043b90ad4b37d3c440171e
AMarceloRamos <adrianramosv@hotmail.com>
2023-05-12 22:49:41 -0400

    subida del proyecto

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # get 'contact/index'
+  get 'contact/create'
+  # get 'projects/index'
+  # get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'projects' <= "projects#index"
+  get 'contact' <= "contact#index"
+  get 'create' <= "contact#create"
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root 'home#index'
+  get '/projects', to: 'projects#index'
+  get '/contact', to: 'contact#index'
+  get '/home', to: 'home#index'
+
+end
