f0439b56d0fc341986b67213584979295d87fd5d
AMarceloRamos <adrianramosv@hotmail.com>
2023-05-15 11:02:00 -0400

    formulario_con problemas por corregir(metodo get)

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,6 +1,9 @@
 class ContactController < ApplicationController
   def index
+    @contact =Contact.all
   end
   def create
+    @contact= Contact.new( name: params[:inputName], email: params[:inputEmail], comments: params[:inputComments]);
+    @contact.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # get 'contact/index'
   get 'contact/create'
+  get 'home/createLogin'
   # get 'projects/index'
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'projects' <= "projects#index"
   get 'contact' <= "contact#index"
   get 'create' <= "contact#create"
+
   # Defines the root path route ("/")
   # root "articles#index"
 
